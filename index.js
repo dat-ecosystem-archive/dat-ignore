18e7c20f20a1b8df966701a4beb523f7125b083d
Joe Hand <joe@hand.email>
2017-03-27 11:35:13 -0700

    add options add tests

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
-var assert = require('assert')
 var fs = require('fs')
 var path = require('path')
 var match = require('anymatch')
+var xtend = require('xtend')
 
 module.exports = ignore
 
-function ignore (dir) {
-  var opts = {
-    datignore: path.join(dir, '.datignore')
+function ignore (dir, opts) {
+  if (typeof dir !== 'string') {
+    opts = dir
+    dir = null
   }
-  opts.ignore = opts.ignore // we want an array here
+  opts = xtend({
+    datignorePath: dir ? path.join(dir, '.datignore') : '.datignore'
+  }, opts)
+
+  var ignoreMatches = opts.ignore // we end up with array of ignores here
     ? Array.isArray(opts.ignore)
       ? opts.ignore
       : [opts.ignore]
     : []
-  var defaultIgnore = [/^(?:\/.*)?\.dat(?:\/.*)?$/] // ignore .dat
-  var ignoreHidden = [/[/\\]\./]
-  var datIgnore = !(opts.useDatIgnore === false) ? readDatIgnore() : []
+
+  var defaultIgnore = [/^(?:\/.*)?\.dat(?:\/.*)?$/, '.DS_Store'] // ignore .dat (and DS_Store)
+  var ignoreHidden = !(opts.ignoreHidden === false) ? [/(^\.|\/\.).*/] : null // ignore hidden files anywhere
+  var datIgnore = !(opts.useDatIgnore === false) ? readDatIgnore() : null
 
   // Add ignore options
-  opts.ignore = opts.ignore.concat(defaultIgnore) // always ignore .dat folder
-  if (datIgnore) opts.ignore = opts.ignore.concat(datIgnore) // add .datignore
-  if (opts.ignoreHidden !== false) opts.ignore = opts.ignore.concat(ignoreHidden) // ignore all hidden things
+  ignoreMatches = ignoreMatches.concat(defaultIgnore) // always ignore .dat folder
+  if (datIgnore) ignoreMatches = ignoreMatches.concat(datIgnore) // add .datignore
+  if (ignoreHidden) ignoreMatches = ignoreMatches.concat(ignoreHidden) // ignore all hidden things
 
   return function (file) {
-    return match(opts.ignore, file)
+    return match(ignoreMatches, file)
   }
 
   function readDatIgnore () {
     try {
-      return fs.readFileSync(opts.datignore, 'utf8')
+      var ignores = opts.datignore || fs.readFileSync(opts.datignorePath, 'utf8')
+      if (ignores && typeof opts.datignore !== 'string') ignores = ignores.toString()
+      return ignores
+        .trim()
         .split('\n')
         .filter(function (str) {
           return !!str.trim()
